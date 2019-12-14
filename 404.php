6b768d9f0b64ac7f230c76f28a2f2e2bee947c70
aheeeeeeed <ahmed.benmbarek96@gmail.com>
2019-12-14 22:35:08 +0100

    change 404 page language to french

--- 404.php
+++ 404.php
@@ -31,12 +31,12 @@
 			<div class="notfound-404">
 				<h1>Oops!</h1>
 			</div>
-			<h2>404 - Page not found</h2>
-			<p>The page you are looking for might have been removed had its name changed or is temporarily unavailable.</p>
-			<a href="index.html">Go To Homepage</a>
+			<h2>404 - Page non trouvée !</h2>
+			<p>La page que vous recherchez a peut-être été supprimée si son nom a changé ou est temporairement indisponible.</p>
+			<a href="index.html">Retourner vers la première page</a>
 		</div>
 	</div>
 
-</body><!-- This templates was made by Colorlib (https://colorlib.com) -->
+</body>
 
 </html>
